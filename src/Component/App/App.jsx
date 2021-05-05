f5a7bbc5c31ace821606bbd1e2737c3e57dc3694
prathyu <prathyuprasad04669@gmail.com>
2021-05-05 12:37:24 +0530

    landing section complted

--- src/Component/App/App.jsx
+++ src/Component/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Landing from '../Landing/Landing'
 import Nav from '../Nav/Nav'
 import './App.css'
 
@@ -8,6 +9,7 @@ const App = () => {
           {  /* <Nav Section />  */}
            <Nav />
           {  /* <Landing page/>  */}
+          <Landing />
 
           {  /* <Challenge Section/>  */}
 
