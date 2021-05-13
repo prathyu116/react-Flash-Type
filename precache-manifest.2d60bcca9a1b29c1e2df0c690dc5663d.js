082e91667d5e527879b538550c64b835fa3a2fb1
prathyu <prathyuprasad04669@gmail.com>
2021-05-14 01:34:06 +0530

    Updates

---
+++ precache-manifest.2d60bcca9a1b29c1e2df0c690dc5663d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9cb33b816230c3c1e2ca47ec52b8f8",
+    "url": "/react-Flash-Type/index.html"
+  },
+  {
+    "revision": "4bd6ab15f46ba523a9ab",
+    "url": "/react-Flash-Type/static/css/main.a7373baf.chunk.css"
+  },
+  {
+    "revision": "896809905855ad626e59",
+    "url": "/react-Flash-Type/static/js/2.f9ea682a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Flash-Type/static/js/2.f9ea682a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd6ab15f46ba523a9ab",
+    "url": "/react-Flash-Type/static/js/main.978406ad.chunk.js"
+  },
+  {
+    "revision": "d3d2c5ad739512a7ce53",
+    "url": "/react-Flash-Type/static/js/runtime-main.ccf10577.js"
+  },
+  {
+    "revision": "6c0b8d67b7513c80189983058e0ac98a",
+    "url": "/react-Flash-Type/static/media/hero.6c0b8d67.png"
+  },
+  {
+    "revision": "f6028f313be8af8349796f0406a2566d",
+    "url": "/react-Flash-Type/static/media/logo.f6028f31.png"
+  }
+]);
